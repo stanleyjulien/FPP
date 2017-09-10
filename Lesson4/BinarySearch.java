b7b70bc14994b828ab60a4b133ac4918a733ab9a
stanleyjulien <tijesus151@gmail.com>
2017-09-10 00:35:11 -0500

    Add files via upload

--- Lesson4/BinarySearch.java
+++ Lesson4/BinarySearch.java
@@ -4,7 +4,9 @@ public class BinarySearch
 {
 	public static int binarySearch(int[] intArr, int key, int min, int max)
 	{
+		// Array must be sorted
 		int mid = (min+max)/2;
+		// key must not be lower than the first value and not be upper than the last value of the array. In case of, a value -1 will return
 		if((key>intArr[intArr.length-1]||(key<intArr[0])))
 		{
 			return -1;
